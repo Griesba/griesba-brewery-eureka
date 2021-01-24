67d4b65eee9f15116359a4c25b2cb7d329e13ff8
griesba <griesba.pat@hotmail.com>
2021-01-24 15:15:49 +0100

    Setup eureka server config

--- src/main/java/com/griesba/brewery/eureka/GriesbaBreweryEurekaApplication.java
+++ src/main/java/com/griesba/brewery/eureka/GriesbaBreweryEurekaApplication.java
@@ -2,7 +2,9 @@ package com.griesba.brewery.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class GriesbaBreweryEurekaApplication {
 
