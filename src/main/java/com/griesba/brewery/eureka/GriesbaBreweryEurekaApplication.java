99f3f57e7488754729ebd9190baaff9c0ac54b09
Jean Griesbacher <jnsmplcpt@gmail.com>
2021-01-24 15:10:46 +0100

    Project setup

---
+++ src/main/java/com/griesba/brewery/eureka/GriesbaBreweryEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.griesba.brewery.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriesbaBreweryEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GriesbaBreweryEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/griesba/brewery/eureka/GriesbaBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griesba.brewery.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GriesbaBreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
